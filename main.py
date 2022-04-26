ab81968031ed2abb4fedcea32ed2089de8a55b94
tristan7491 <titou.lailler@gmail.com>
2022-04-26 17:44:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-import pygame
+from random import randint
 
-print("Hello")
-print("ca va")
+import pygame
+pygame.init()
+clock = pygame.time.Clock()
+
+pygame.display.set_caption("Multigame")
+screen = pygame.display.set_mode((1080, 650))
+r = v = b = 0
+
+while(not (pygame.event.peek(pygame.QUIT))):
+    screen.fill(pygame.Color(r, v, b))
+    r = round(randint(0,255))
+    v = round(randint(0,255))
+    b = round(randint(0,255))
+    
+    pygame.display.flip()
+    clock.tick(5)
+    
+pygame.quit()
